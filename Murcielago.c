12d3d19c4126ba2e12c6be30306ed22b886cc353
Kathya Alvarez <kathyap.alvarez@gmail.com>
2018-03-12 17:22:57 -0600

    Add a new file with murcielago encryptation.

---
+++ Murcielago.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include "Murcielago.h" 
+char* murcielago (char* word ) {
+	char message [100];
+	strcpy (message, word);
+	 
+	for (int c=0; c<=100; c++) {
+	 
+		switch (message[c]) {
+		{
+		case 'm':
+			message[c]='0';
+			break;
+		}
+		{
+		case 'u':
+			message[c]='1';
+			break;
+		}
+		{
+		case 'r':
+			message[c]='2';
+			break;
+		}
+		{
+		case 'c':
+			message[c]='3';
+			break;
+		}
+		{
+		case 'i':
+			message[c]='4';
+			break;
+		}
+		{
+		case 'e':
+			message[c]='5';
+			break;
+		}
+		{
+		case 'l':
+			message[c]='6';
+			break;
+		}
+		{
+		case 'a':
+			message[c]='7';
+			break;
+		}
+		{
+		case 'g':
+			message[c]='8';
+			break;
+		}
+		{
+		case 'o':
+			message[c]='9';
+			break;
+		}
+		default:
+			message[c]=message[c];
+		}
+	}
+	return message;
+}
--- hello.c
+++ hello.c
@@ -8,11 +8,12 @@
 
 #include "palindrom.h"
 #include "anagrama.h"
+#include "Murcielago.h"
 
 int main()
 {
 	char string[256]={0};
-	char a[100], b[100];
+	char a[100], b[100], c[100], d[100];
 	int flag;
 
 	printf("Please, introduce a string\n");
@@ -28,7 +29,10 @@ int main()
 		printf("%s and %s are anagrams\n", a, b);
 	else
 		printf("%s and %s are not anagrams.\n", a, b);
-
+	printf("Provide another word");
+	gets(c);
+	strcpy (d, murcielago(c));
+        printf("%s", d);
 	return 0;
 }
    
